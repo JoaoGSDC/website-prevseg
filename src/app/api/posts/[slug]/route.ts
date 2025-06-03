505fd84d9a2971f69ed88ab0979abd9770fde114
JoaoGSDC <joaogsdc@gmail.com>
2025-06-03 11:46:23 -0300

    fix: change slug endpoint route

--- src/app/api/posts/[slug]/route.ts
+++ src/app/api/posts/[slug]/route.ts
@@ -1,10 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { NextRequest, NextResponse } from 'next/server';
 import { Db } from 'mongodb';
 import connectToDatabase from '../../utils/dbConnect';
 
-export async function GET(req: NextRequest, context: { params: { slug: string } }) {
+export async function GET(_: NextRequest, context: any) {
   try {
     const db: Db = await connectToDatabase();
+
     const { slug } = context.params;
 
     const post = await db.collection('posts').findOne({ slug });
