33fcd915d5033ebbc264dc8200251816fc1f610e
JoaoGSDC <joaogsdc@gmail.com>
2025-06-25 23:44:34 -0300

    fix: implemented adjustments in blog interface

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -5,15 +5,7 @@ import React, { useEffect, useState } from 'react';
 import styles from './styles.module.scss';
 import Image from 'next/image';
 import Link from 'next/link';
-import {
-  FaFacebook,
-  FaInstagram,
-  FaMagnifyingGlass,
-  FaMapLocationDot,
-  FaRegMessage,
-  FaBars,
-  FaWhatsapp,
-} from 'react-icons/fa6';
+import { FaFacebook, FaInstagram, FaMapLocationDot, FaRegMessage, FaBars, FaWhatsapp } from 'react-icons/fa6';
 import { scrollToSection } from '@/utils/scrollToSection';
 import { IoClose } from 'react-icons/io5';
 
@@ -79,7 +71,12 @@ const Navbar = () => {
           </div>
 
           <button>
-            <FaMagnifyingGlass />
+            <a
+              href="https://wa.me/551934426055?text=Ol%C3%A1%2C%20gostaria%20de%20saber%20mais%20sobre%20os%20servi%C3%A7os%20da%20PrevSeg"
+              target="_blank"
+            >
+              <FaWhatsapp />
+            </a>
           </button>
         </div>
       </div>
